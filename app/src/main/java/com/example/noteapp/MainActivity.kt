ba125899b560a637c813c11030eab98298ca2550
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-10 15:42:35 -0300

    Setup recycler View on Main

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
+import com.example.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var noteAdapter: NoteAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
 
         val db = Room.databaseBuilder(
             applicationContext,
@@ -18,5 +27,11 @@ class MainActivity : AppCompatActivity() {
         val noteDao = db.noteDao()
 
         //val note = noteDao.getAll()
+    }
+
+    private fun setupRecyclerView() = binding.rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
