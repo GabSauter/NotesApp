3678f17a781e0aa866f168bca2cd3789bbcc836d
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-10 14:36:43 -0300

    Initialize database on Main

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            NoteDatabase::class.java,
+            "note-database"
+        ).build()
+
+        val noteDao = db.noteDao()
+
+        //val note = noteDao.getAll()
     }
 }
