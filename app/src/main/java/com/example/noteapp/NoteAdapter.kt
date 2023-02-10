eb05b7c87967c6b189025ea67b930dcad0b400de
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-02-10 15:08:30 -0300

    Create the RecyclerView Adapter

---
+++ app/src/main/java/com/example/noteapp/NoteAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.noteapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.databinding.NoteBinding
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(val binding: NoteBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Note>(){
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+    private var notes: List<Note>
+        get() = differ.currentList
+        set(value) {differ.submitList(value)}
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(NoteBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.binding.apply {
+            val note = notes[position]
+            tvTitle.text = note.title
+        }
+    }
+}
